13bdbc6739d8f90c04f87c899fd0c3058243a141
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2021-10-10 14:00:24 +0000

    simple jsx practice

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import Example from './example';
+
+function Food({fav}){
+    return <h3>I like {fav}</h3>;
+}
 
 function App() {
   return (
       <div>
-        <h1>I fucking hate this country</h1>
-        <Example />
+        <h1>Please send me to the States</h1>
+        <Food fav="1$ pizza" />
+        <Food fav="chipotle" />
+        <Food fav="whataburger" />
+        <Food fav="taco" />
       </div>
   );
 }
--- movie_app_2021/src/example.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Example(){
-    return <h3>Send Me Home!!!</h3>;
-}
-
-export default Example;
