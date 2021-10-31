1e5e9522e43c06ecbcbafe3a8ac0705403ead631
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2021-10-31 14:38:00 +0000

    minor edits

--- movie_app_2021/src/sub-header.js
+++ movie_app_2021/src/sub-header.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './sub-header.css';
+
+function Examples({ex}){
+    return(
+        <p>miss the {ex}...</p>
+    );
+}
 
 function Subheader(){
     return(
         <div>
             <h2>I hate this MF country</h2>
             <h2>Send me home!!!</h2>
+            <Examples ex="food" />
+            <Examples ex="people" />
+            <Examples ex="vibe" />
         </div>
     );
 }
